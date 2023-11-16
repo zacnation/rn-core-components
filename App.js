a802a329bb8b903715ada6941626fd0a0a0a730f
Zac Nation <zac.nation@icloud.com>
2023-11-16 09:18:47 +0000

    Activity Indicator + count game

--- App.js
+++ App.js
@@ -9,20 +9,33 @@ import {
   Pressable,
   Modal,
   StatusBar,
+  ActivityIndicator,
 } from "react-native";
 
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
-  const [isStatusBarHidden, setIsStatusBarHidden] = useState(false);
+  const [isLoadingVisible, setIsLoadingVisible] = useState(true);
+  const [count, setCount] = useState(0);
   return (
     <View style={{ flex: 1, backgroundColor: "teal", padding: 60 }}>
-      <StatusBar barStyle="light-content" hidden={isStatusBarHidden} />
-      <Button
-        title="Toggle Status Bar"
-        color="orange"
-        onPress={() => setIsStatusBarHidden(!isStatusBarHidden)}
+      <ActivityIndicator
+        size="large"
+        style={{ paddingTop: 100, paddingBottom: 20 }}
+        animating={isLoadingVisible}
       />
+      <Button
+        title="Toggle Loading Icon"
+        color="orange"
+        onPress={() => setIsLoadingVisible(!isLoadingVisible)}
+      />
+      <Button title="Add" color="hotpink" onPress={() => setCount(count + 1)} />
+      <Button
+        title="Subtract"
+        color="hotpink"
+        onPress={() => setCount(count - 1)}
+      />
+      <Text style={{ fontSize: 30 }}>{count}</Text>
     </View>
   );
 }
